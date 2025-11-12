276479d2e93ae69925aabb257d8dfef0035c690a
mostafatamer123 <mostafatam81@gmail.com>
2025-11-12 15:11:28 +0200

    update the dashboard

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReportsService } from './reports.service';
 import { ReportsController } from './reports.controller';
-import { ReportSnapshot, Appointment, AgentPayment, User, Conversion, VisitorTracking } from 'entities/global.entity';
+import { ReportSnapshot, Appointment, AgentPayment, User, Conversion, VisitorTracking, Property } from 'entities/global.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ReportSnapshot, Appointment, AgentPayment, User, Conversion, VisitorTracking])],
+  imports: [TypeOrmModule.forFeature([ReportSnapshot, Appointment, AgentPayment, User, Conversion, VisitorTracking,Property])],
   controllers: [ReportsController],
   providers: [ReportsService],
   exports: [ReportsService],
